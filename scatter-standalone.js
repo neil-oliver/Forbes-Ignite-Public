e22d47616cea40ebb201ca7d6637011e1aeca74b
neil-oliver <neiloliverphoto@gmail.com>
2022-01-03 19:21:56 +0000

    Update scatter-standalone.js

--- scatter-standalone.js
+++ scatter-standalone.js
@@ -423,7 +423,8 @@
 
 
         } else if (stage == 3) {
-
+            pointsOut()
+            lineOut()
             points.transition().attr('cy', d => yScale(d.predicted))
             tooltip.text("The predicted score of each study group is plotted along the y-axis.")
 
