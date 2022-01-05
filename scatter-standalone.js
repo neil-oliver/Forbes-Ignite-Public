8d9fa65d49f915efa87977898c224824332c9d87
Marisa Ruiz Asari <marisaruizasari@Marisas-MacBook-Pro.local>
2022-01-05 17:10:19 +0100

    remove console logs'

--- scatter-standalone.js
+++ scatter-standalone.js
@@ -408,9 +408,6 @@
 
     function walkthrough() {
 
-        console.log(stage)
-        console.log('test')
-
         d3.select("#demoButton").text(stage == 0 ? "How do I interpret this graph?" : "Okay I understand now")
         d3.select("#backButton").attr("disabled", stage < 2 ? true : null)
         d3.select("#forwardButton").attr("disabled", stage > 7 ? true : null)
